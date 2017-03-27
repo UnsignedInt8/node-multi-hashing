ed581998395c82984efdec3f723e6d947926c796
UnsignedInt8 <unsignedint8@MacBook-Pro.local>
2017-03-27 11:27:32 +0800

    Add new method declarations

--- node-multi-hashing.d.ts
+++ node-multi-hashing.d.ts
@@ -6,6 +6,23 @@ declare module MutliHashing {
     export function x11(data: any, ...args: any[]): Buffer;
     export function quark(data: any, ...args: any[]): Buffer;
     export function yescrypt(data: any, ...args: any[]): Buffer;
+    export function x13(data: any, ...args: any[]): Buffer;
+    export function x15(data: any, ...args: any[]): Buffer;
+    export function nist5(data: any, ...args: any[]): Buffer;
+    export function keccak(data: any, ...args: any[]): Buffer;
+    export function blake(data: any, ...args: any[]): Buffer;
+    export function skein(data: any, ...args: any[]): Buffer;
+    export function groestl(data: any, ...args: any[]): Buffer;
+    export function fugue(data: any, ...args: any[]): Buffer;
+    export function shavite3(data: any, ...args: any[]): Buffer;
+    export function hefty1(data: any, ...args: any[]): Buffer;
+    export function qubit(data: any, ...args: any[]): Buffer;
+    export function yescrypt(data: any, ...args: any[]): Buffer;
+    export function s3(data: any, ...args: any[]): Buffer;
+    // export function lyra2re(data: any, ...args: any[]): Buffer;
+    export function neoscrypt(data: any, ...args: any[]): Buffer;
+    export function dcrypt(data: any, ...args: any[]): Buffer;
+    
 }
 
 export = MutliHashing;
